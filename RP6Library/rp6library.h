77f95966cc7a28ef7e2de25f2106618e387c6153
Maximilian Voss <voss@adobe.com>
2014-12-03 15:37:07 +0100

    mods

--- RP6Library/rp6library.h
+++ RP6Library/rp6library.h
@@ -1,61 +1,33 @@
 #ifndef __RP6LIBRARY_H__
 #define __RP6LIBRARY_H__
 
-#include <./acs/acs.h
->
-#include <./acs/ircommSend.h
->
-#include <./acs/states.h
->
-#include <./acs/scheduler.h
->
-#include <./acs/acsSend.h
->
-#include <./acs/ircommRecv.h
->
-#include <./core/core.h
->
-#include <./core/bumper.h
->
-#include <./core/initialization.h
->
-#include <./core/RP6.h
->
-#include <./core/buttons.h
->
-#include <./core/rc5data.h
->
-#include <./core/leds.h
->
-#include <./uart/uart.h
->
-#include <./ircomm/recv.h
->
-#include <./ircomm/send.h
->
-#include <./ircomm/ircomm.h
->
-#include <./engine/rotation.h
->
-#include <./engine/scheduler.h
->
-#include <./engine/config.h
->
-#include <./engine/hardware.h
->
-#include <./engine/vector.h
->
-#include <./engine/engine.h
->
-#include <./ircommAcs/ircommAcs.h
->
-#include <./scheduler/scheduler.h
->
-#include <./powermgmt/powermgmt.h
->
-#include <./powermgmt/overcurrent.h
->
-#include <./rp6library.h
->
+#include <./acs/acs.h>
+#include <./acs/ircommSend.h>
+#include <./acs/states.h>
+#include <./acs/scheduler.h>
+#include <./acs/acsSend.h>
+#include <./acs/ircommRecv.h>
+#include <./core/core.h>
+#include <./core/bumper.h>
+#include <./core/initialization.h>
+#include <./core/RP6.h>
+#include <./core/buttons.h>
+#include <./core/rc5data.h>
+#include <./core/leds.h>
+#include <./uart/uart.h>
+#include <./ircomm/recv.h>
+#include <./ircomm/send.h>
+#include <./ircomm/ircomm.h>
+#include <./engine/rotation.h>
+#include <./engine/scheduler.h>
+#include <./engine/config.h>
+#include <./engine/hardware.h>
+#include <./engine/vector.h>
+#include <./engine/engine.h>
+#include <./ircommAcs/ircommAcs.h>
+#include <./scheduler/scheduler.h>
+#include <./powermgmt/powermgmt.h>
+#include <./powermgmt/overcurrent.h>
+#include <./rp6library.h>
 
 #endif
